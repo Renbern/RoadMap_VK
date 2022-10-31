2b899dbd45e2776d79dfcc0eb4d961c14a056daf
Renbern <ArthurRenbern@icloud.com>
2022-10-31 21:43:25 +0300

    develop

---
+++ RoadMap_VK/RoadMap_VK/ViewController.swift
@@ -0,0 +1,11 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Главный экран приложения
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
