c8c2225b2de828e9d60ff629350a7c58c6e182ce
Renbern <ArthurRenbern@icloud.com>
2022-12-14 07:46:26 +0300

    add flexible height for textContent

--- RoadMap_VK/RoadMap_VK/Cells/TextPostTableViewCell.swift
+++ RoadMap_VK/RoadMap_VK/Cells/TextPostTableViewCell.swift
@@ -7,7 +7,7 @@ import UIKit
 final class TextPostTableViewCell: UITableViewCell, PostConfigurable {
     // MARK: - Private visual elements
 
-    @IBOutlet var postTextView: UITextView!
+    @IBOutlet var postTextLabel: UILabel!
 
     // MARK: - Private properties
 
@@ -16,43 +16,7 @@ final class TextPostTableViewCell: UITableViewCell, PostConfigurable {
     // MARK: - Public methods
 
     func configure(post: Post, photoCacheService: PhotoCacheService) {
-        postTextView.text = post.text
-        postTextView.sizeToFit()
-        setupTextContent()
-    }
-
-    // MARK: - Private methods
-
-    private func getLabelSize(text: String, font: UIFont) -> CGSize {
-        let maxWidth = bounds.width - instets * 2
-        let textBlock = CGSize(
-            width: maxWidth,
-            height:
-            CGFloat.greatestFiniteMagnitude
-        )
-        let rect = text.boundingRect(
-            with: textBlock,
-            options:
-            .usesLineFragmentOrigin,
-            attributes: [NSAttributedString.Key.font: font],
-            context: nil
-        )
-        let width = Double(rect.size.width)
-        let height = Double(rect.size.height)
-        let size = CGSize(width: ceil(width), height: ceil(height))
-        return size
-    }
-
-    private func setupTextContent() {
-        let textContentSize = getLabelSize(
-            text: postTextView.text,
-            font: postTextView.font ?? UIFont()
-        )
-        let textContentX = (bounds.width - textContentSize.width) / 2
-        let textContentOrigin = CGPoint(x: textContentX, y: instets)
-        postTextView.frame = CGRect(
-            origin: textContentOrigin,
-            size: textContentSize
-        )
+        postTextLabel.text = post.text
+        postTextLabel.numberOfLines = 5
     }
 }
